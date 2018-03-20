447933ab7a3d5c1bfddaafb05f7e14fb42a517ca
Евгений-ПК\Евгений <jeniamasarn94@gmail.com>
2018-03-20 22:40:21 +0300

    +Add New Files

---
+++ Multi_Assembly/Multi_File_Project/Multi_File_Project/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Enterprise;
+namespace Multi_File_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employer employer = new Employer();
+            employer.EmployerInfo();
+            Employee emproyee = new Employee();
+            emproyee.EmployeeInfo();
+            Console.ReadLine();
+        }
+    }
+}
